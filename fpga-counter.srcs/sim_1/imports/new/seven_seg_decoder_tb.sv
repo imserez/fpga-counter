189ab19cbbccafac549e31b15e6231d4ad3586bd
Sergi J. <139800964+imserez@users.noreply.github.com>
2025-12-31 12:16:02 +0100

    feat: add full-support of A-F values in 7-segment display

--- fpga-counter.srcs/sim_1/imports/new/seven_seg_decoder_tb.sv
+++ fpga-counter.srcs/sim_1/imports/new/seven_seg_decoder_tb.sv
@@ -22,21 +22,21 @@
 
 module seven_seg_decoder_tb;
 
-    logic   [3:0] bcd;
+    logic   [3:0] bcde;
     logic   [6:0] segments;
     
     seven_seg_decoder dut (
-        .bcd(bcd),
+        .bcde(bcde),
         .segments(segments)
     );
     
     
     initial begin
-        for (int i = 0; i < 10; i++) begin
+        for (int i = 0; i < 16; i++) begin
         
-            bcd = i;
+            bcde = i;
             #10;
-            $display("BCD=%d -> segments=%b", bcd, segments);
+            $display("BCD=%d -> segments=%b", bcde, segments);
         end
         
         $display("All digits tested");
--- fpga-counter.srcs/sources_1/new/seven_seg_decoder.sv
+++ fpga-counter.srcs/sources_1/new/seven_seg_decoder.sv
@@ -21,7 +21,7 @@
 
 
 module seven_seg_decoder(
-    input   logic [3:0] bcd,        // 0-9 nums
+    input   logic [3:0] bcde,        // 0-15 nums
     output  logic [6:0] segments   // a:g
     );
       
@@ -29,7 +29,7 @@ module seven_seg_decoder(
      // 0 is on, 1 is off
      // combinational circuit, no memory, no clock. Output depends only on input bcd
      always_comb begin
-        case (bcd)
+        case (bcde)
             4'h0: segments = 7'b1000000; // 0
             4'h1: segments = 7'b1111001; // 1
             4'h2: segments = 7'b0100100; // 2
@@ -40,6 +40,13 @@ module seven_seg_decoder(
             4'h7: segments = 7'b1111000; // 7
             4'h8: segments = 7'b0000000; // 8
             4'h9: segments = 7'b0010000; // 9
+            4'hA: segments = 7'b0001000; // A
+            4'hB: segments = 7'b0000011; // B
+            4'hC: segments = 7'b1000110; // C
+            4'hD: segments = 7'b0100001; // D
+            4'hE: segments = 7'b0110000; // E
+            4'hF: segments = 7'b0111000; // F
+
             default: segments = 7'b1111111; // apagado
         endcase
     end
