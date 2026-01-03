f7702a3209987a2ec42a7fa093eb13529a5422dd
Sergi J. <139800964+imserez@users.noreply.github.com>
2026-01-03 15:21:48 +0100

    feat: modified counter_top to apply the debounced signals to speed up/down the clk limit

--- fpga-counter.srcs/sources_1/new/counter_top.sv
+++ fpga-counter.srcs/sources_1/new/counter_top.sv
@@ -24,9 +24,17 @@ module counter_top(
     input   logic clk,      // 100MHz nexys a7
     input   logic reset,    // rst button
     input   logic enable,   // enable button
+    input   logic   speed_up,   // speed_up btn
+    input   logic   speed_down, // speed_down btn
     output  logic [6:0] seg,    // 7-seg
-    output  logic [7:0] an      // display enable, 8 displays
+    output  logic [7:0] an,      // display enable, 8 displays
+    output  logic   db_speed_up,
+    output  logic   db_speed_down
 );
+
+
+    logic   db_up_prev;
+    logic   db_down_prev;
 
     logic   clk_slow;
     logic   clk_segment;
@@ -35,16 +43,36 @@ module counter_top(
     logic   [3:0] bcde_in;
     logic   [7:0] an_enable;
     
+    logic   increment_speed = 0;
+    logic   dynamic_speed = 50_000_000;
+    
+    
+    debouncer db_up (
+        .clk(clk),
+        .signal(speed_up),
+        .reset(reset),
+        .debounced_signal(db_speed_up)
+    );
+    
+    debouncer db_down (
+        .clk(clk),
+        .signal(speed_down),
+        .reset(reset),
+        .debounced_signal(db_speed_down)
+    );
+    
     clock_divider #(.LIMIT(50_000_000), .N(27)) cd (
         .clk(clk),
         .reset(reset),
-        .clk_out(clk_slow)
+        .clk_out(clk_slow),
+        .dynamic_limit(dynamic_speed)
     );
     
     clock_divider #(.LIMIT(100_000), .N(17)) cd_segment (
         .clk(clk),
         .reset(reset),
-        .clk_out(clk_segment)
+        .clk_out(clk_segment),
+        .dynamic_limit(100_000)
     );
     
     seven_seg_selector segment_select (
@@ -94,5 +122,25 @@ module counter_top(
                 bcde_in = count[31:28];
             end
         endcase
+    end
+    
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            dynamic_speed <= 50_000_000;
+            db_up_prev <= 0;
+            db_down_prev <= 0;
+        end
+        else begin
+            if (db_speed_up && !db_up_prev) begin
+                if (dynamic_speed > 5_000_000)
+                    dynamic_speed <= dynamic_speed - 5_000_000;
+            end
+            if (db_speed_down && !db_down_prev) begin
+                if (dynamic_speed < 100_000_000)
+                    dynamic_speed <= dynamic_speed + 5_000_000;
+            end
+            db_up_prev <= db_speed_up;
+            db_down_prev <= db_speed_down;
+        end
     end
 endmodule
